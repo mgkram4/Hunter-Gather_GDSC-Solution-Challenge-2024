4e13a6d15e00563b7ee1dfe56299546620c5721f
Bryan Bergo <bryan.l.bergo@gmail.com>
2024-01-12 15:36:35 -0700

    config fix

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,2 @@
-// Supabase
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
+export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
