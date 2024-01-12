9f5e55f937f1cc56fd686b92988d53c62568fdac
Bryan Bergo <bryan.l.bergo@gmail.com>
2024-01-12 15:08:27 -0700

    husky setup

--- api/supabase.ts
+++ api/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_KEY, SUPABASE_URL } from "@config/constants"
+import { SUPABASE_KEY, SUPABASE_URL } from "@config/constants";
 
 export const supabase = createClient(SUPABASE_KEY, SUPABASE_URL);
