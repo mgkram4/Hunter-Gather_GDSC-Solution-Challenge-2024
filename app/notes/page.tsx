55173ffdd2c9c3f049d56e957335f1a45549bd7f
Bryan Bergo <bryan.l.bergo@gmail.com>
2024-01-12 15:05:39 -0700

    github setup

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,5 @@
-import { supabase } from "api/supabase";
+// This page is a test for supabase setup and will be deleted once we start building the app
+import { supabase } from "@api/supabase";
 
 export default async function Notes() {
   const { data: notes } = await supabase.from("notes").select();
