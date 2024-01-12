42b11ae594fc6e7b76ab76add9ac9127d759d82b
Bryan Bergo <bryan.l.bergo@gmail.com>
2024-01-12 15:01:06 -0700

    supabase setup

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,6 @@
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
+import { supabase } from "api/supabase";
 
 export default async function Notes() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
   const { data: notes } = await supabase.from("notes").select();
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
---
+++ config/constants.ts
@@ -0,0 +1,3 @@
+// Supabase
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
