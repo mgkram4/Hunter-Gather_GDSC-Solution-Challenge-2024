3745a64d4b5d37a955ecebf3d3d4689249deec5b
yz <youssef.zerroug1@gmail.com>
2024-01-19 20:48:19 -0800

    ui structuring fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,16 @@
+/*
+#TODO
+- [ ] Display dummy user profile stats
+- [ ] Fetch user profile data from Supabase
+- [ ] Fetch user taste data from Supabase
+- [ ] Display fetched data
+- [ ] Figure out the recipes/posts section
+
+#NOTE
+  import { createClient } from "@utils/supabase/server";
+  or import { createClient } from @utils/supabase/client ?
+*/
+
 // Dummy data types
 interface UserProfile {
   username: string;
@@ -21,7 +34,7 @@ const UserProfileInfo = ({
   tasteProfile,
 }: UserProfile) => {
   return (
-    <div className="flex flex-row items-center justify-center">
+    <div className="flex flex-row items-center justify-evenly">
       {/* Profile picture, username, handle, and buttons */}
       <div className="flex flex-col items-center">
         <img
@@ -34,7 +47,7 @@ const UserProfileInfo = ({
           <p className="text-gray-500">{handle}</p>
         </div>
         {isCurrentUser && (
-          <div className="flex mt-4 md:mt-0">
+          <div className="flex mt-4 md:mt-0 p-8">
             <button className="bg-green-400 text-white px-4 py-2 rounded-lg mr-2">
               Settings
             </button>
@@ -46,33 +59,35 @@ const UserProfileInfo = ({
       </div>
 
       {/* Stats and Taste Profile */}
-      <div className="mt-4 md:mt-0 md:flex md:items-center md:space-x-6">
-        <div className="flex justify-between space-x-4">
+      <div className="mt-4 md:mt-0 md:flex md:items-center md:space-x-6 flex-wrap flex-col">
+        <div className="flex justify-between space-x-4 m-4 p-2">
           {/* Stats */}
-          <div className="text-center">
+          <div className="text-center m-4 pl-2 pr-2">
             <p className="text-lg font-bold">Recipes</p>
             {/* Number of recipes */}
+            <p className="text-base font-bold">{}</p>
           </div>
-          <div className="text-center">
+          <div className="text-center m-4 pl-2 pr-2">
             <p className="text-lg font-bold">Followers</p>
             {/* Number of followers */}
           </div>
-          <div className="text-center">
+          <div className="text-center m-4 pl-2 pr-2">
             <p className="text-lg font-bold">Following</p>
             {/* Number of followings */}
           </div>
         </div>
-        {/* Taste Tags */}
-        <div className="flex flex-wrap mt-4 md:mt-0">
-          {tasteProfile?.tastes.map((taste, index) => (
-            <div
-              key={index}
-              className="bg-green-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2"
-            >
-              #{taste}
-            </div>
-          ))}
-        </div>
+        <div className="flex justify-between space-x-4 m-4 p-2">{bio}</div>
+      </div>
+      {/* Taste Tags */}
+      <div className="flex flex-wrap mt-4 md:mt-0">
+        {tasteProfile?.tastes.map((taste, index) => (
+          <div
+            key={index}
+            className="bg-green-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2"
+          >
+            #{taste}
+          </div>
+        ))}
       </div>
     </div>
   );
