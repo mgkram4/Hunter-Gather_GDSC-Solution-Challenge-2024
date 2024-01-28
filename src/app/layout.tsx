73155fdb5599309df2eab0c0e33ea38e94e07d48
blbergo <bryan.l.bergo@gmail.com>
2024-01-28 14:26:21 -0800

    vercel build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@components/navbar";
-import Sidebar from "./homepage/sidebar";
+import Footer from "../components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
